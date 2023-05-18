78de9f10111c4f3e4f11d09a7d94e2cb3680b295
kacper <kacperc317@gmail.com>
2023-05-19 01:27:10 +0200

    Optymalzacjia kodu

--- Avalonia3/Views/MainWindow.axaml.cs
+++ Avalonia3/Views/MainWindow.axaml.cs
@@ -18,29 +18,27 @@ namespace Avalonia3.Views
 {
     public partial class MainWindow : Window
     {
-        MainModelView modelView;
+        MainModelView ModelView;
         public MainWindow()
         {
             InitializeComponent();
             ((IClassicDesktopStyleApplicationLifetime)Avalonia.Application.Current.ApplicationLifetime).MainWindow = this;
             DataContextChanged += OnDataContextChanged;
         }
-
         private async void OnDataContextChanged(object sender, EventArgs e)
         {
             if (DataContext != null)
             {
-                modelView = (MainModelView)DataContext;
+                ModelView = (MainModelView)DataContext;
              
                 TabLoaded(null, null);
             }
         }
-
         public void TabLoaded(object sender, EventArgs e)
         {
-            if (modelView != null)
+            if (ModelView != null)
             {
-                modelView.CreateTab(new Models.TabItemContent() {Text = ""});
+                ModelView.CreateTab(new Models.TabItemContent() {Text = ""});
             }
             else
             {
@@ -48,25 +46,13 @@ namespace Avalonia3.Views
                 // ...
             }
         }
-
         private void Remove_LeftButtondDown(object sender, PointerPressedEventArgs args)
         {
             if (sender is Image img)
             {
-                // Wykonaj ¿¹dane dzia³ania, korzystaj¹c z w³aœciwoœci Tag elementu TabItem
-                //int i = int.Parse((img.Tag as Models.TabItemContent).Tag.ToString());
-
                 TabControl tabControl = this.FindControl<TabControl>("tabControl") as TabControl;
-
                 tabControl.Items = new AvaloniaList<TabItemContent>();
-
-             
-              
-
-
-
             }
-
         }
 
     }
