3cf67f038122258b8d701c7000ab97ef52a0a4d8
Kacper Chmielowiec <104657546+KacperChmielowiec@users.noreply.github.com>
2023-06-08 21:46:07 +0200

    Delete json-formatter-main directory

--- json-formatter-main/src/JsonFormatter/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JsonFormatter;
-
-public static class Constants
-{
-    public const short IndentationWidth = 35;
-    public const int MaxInputLength = 40000;
-    public const int MaxNodeCount = 2000;
-}
