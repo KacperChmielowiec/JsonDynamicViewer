30f0c974cb081b7bb8c5bff5953b2c4195065555
Kacper Chmielowiec <104657546+KacperChmielowiec@users.noreply.github.com>
2023-06-08 21:29:42 +0200

    Delete Avalonia3-master2 directory

--- Avalonia3-master2/Avalonia3/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Avalonia3;
-
-public static class Constants
-{
-    public const short IndentationWidth = 35;
-    public const int MaxInputLength = 40000;
-    public const int MaxNodeCount = 2000;
-}
